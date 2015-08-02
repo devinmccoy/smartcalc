ceabcc08b6ffec951b455a87f8e381c92b730699
Devin McCoy <devink1999@gmail.com>
2015-08-02 14:11:26 -0700

    commit

---
+++ basiccalc.py
@@ -0,0 +1,19 @@
+def add(x,y):
+	x = x + y
+	print(x)
+	return(x)
+	
+def subtract(x,y):
+	x = x - y
+	print(x)
+	return(x)
+	
+def multiply(x,y):
+	x = x*y
+	print(x)
+	return(x)
+def divide(x,y):
+	x = x/y
+	print(x)
+	return(x)
+	
